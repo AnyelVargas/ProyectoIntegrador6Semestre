63bb69ca31b2136c9db7c052fa6663cbcfc09a08
AnyelVargas <101644826+AnyelVargas@users.noreply.github.com>
2022-03-22 17:48:12 -0500

    Se agrega el java para la interfaz

---
+++ src/proyectointegrador6semestre/ProyectoIntegrador6Semestre.java
@@ -0,0 +1,19 @@
+
+/*
+Anyel Vargas
+Daniel Mora
+Guillermo Guerrero
+Sebastian Henao
+Andres Cruz
+*/
+
+package proyectointegrador6semestre;
+
+public class ProyectoIntegrador6Semestre {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
